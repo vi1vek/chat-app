ce2ec6ef052c322512d33118bb77a40cbee2870e
vi1vek <vivekumardhnairtel@gmail.com>
2026-03-08 15:55:47 +0530

    add Image feature

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const {Server} =require('socket.io')
 const app = express()
 app.use(express.static('public'))
 const server = createServer(app)
-const io = new Server(server)
+const io = new Server(server,{
+    maxHttpBufferSize: 1e7
+})
 
 app.get('/',(req,res)=>{
     return res.sendFile('index.html')
@@ -13,10 +15,16 @@ app.get('/',(req,res)=>{
 
 io.on('connection',(Socket)=>{
     console.log(`A user connected : ${Socket.id}`);
-    Socket.on("message",(msg)=>{
-        // console.log(msg);
-        io.emit("message",msg,Socket.id)
+    Socket.on("chat-message",(data)=>{
+        // console.log(data.message);
+        // console.log(data.image);
+        io.emit("receive-chat",{
+            message:data.message,
+            image:data.image
+        },Socket.id)
+        
     })
+    
 })
 server.listen(3000,()=>{
     console.log(`server is running at 3000 port`);
